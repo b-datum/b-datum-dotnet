c8419b7e5b972d78fd1ff5fda3f60266b2ee2276
unknown <Frederico@Frederico-PC.(none)>
2012-11-14 14:42:32 -0200

    Add new upload method and md5sum hashing

--- bdatum/bdatum.cs
+++ bdatum/bdatum.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.IO;
 using System.Net;
 using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
 
 namespace bdatum
 {
@@ -86,6 +88,100 @@ namespace bdatum
             string responseFromServer = response_stream.ReadToEnd();
 
             return responseFromServer;
+        }
+
+        private static string _GetMd5HashFromFile(string filename)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                var buffer = md5.ComputeHash(File.ReadAllBytes(filename));
+                var sb = new StringBuilder();
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    sb.Append(buffer[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /*
+         *   Hand made post, the dot net client wasn´t working ok so 
+         *   I wrote it byhand to help debug and fine adjust
+         */ 
+
+        public static long UPLOAD(string path, string auth_key, string file)
+        {
+
+            NameValueCollection nvc = new NameValueCollection();
+
+            String file_hash = _GetMd5HashFromFile(file).ToUpper();          
+            
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url + "/storage/"  + path );
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, "value", file, "multipart/form-data");
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            try
+            {
+                // clean webbrowser                
+
+                wresp = wr.GetResponse();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                var content = reader2.ReadToEnd();
+
+            }
+            catch (Exception ex)
+            {
+
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
+
+            return 0;
         }
     }
 
